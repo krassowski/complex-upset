8d1d3345f6d80de3a5f64dc9abc44348c2594e22
krassowski <krassowski.michal@gmail.com>
2020-12-04 20:51:28 +0000

    Update spellcheck and CRAN build scripts

--- scripts/package_for_cran.sh
+++ scripts/package_for_cran.sh
@@ -1,12 +1,15 @@
 rm -fr cran_package
 mkdir cran_package
-for folder in R vignettes man;
+for folder in R man;
     do cp -r ${folder} cran_package/;
 done;
-for file in DESCRIPTION LICENSE NAMESPACE README.md;
+mkdir cran_package/vignettes
+cp -r vignettes/Examples_R_files cran_package/vignettes/
+cp -r vignettes/Examples_R.Rmd cran_package/vignettes/
+for file in DESCRIPTION LICENSE NAMESPACE README.md .Rbuildignore;
     do cp ${file} cran_package/;
 done;
 cd cran_package
 ../scripts/cmd_check.sh
 ../scripts/spell_check.sh
-R CMD build
+R CMD build .
